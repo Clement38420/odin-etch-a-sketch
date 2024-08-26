23aa4584161aca1ff41a41c2b359c6b5e863ea7f
Clement Charbonnel <c.charbo38@gmail.com>
2024-08-26 19:58:44 +0200

    ADD THE SIZE BUTTON

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const grid = document.querySelector(".grid");
+const setSizeButton = document.querySelector("button.set-size")
 
 let gridSize = 16;
 
-document.addEventListener("DOMContentLoaded", () => {
+function colorSquare(e) {
+    e.target.style.background = "black";
+}
+
+function clearGrid() {
+    let lines = grid.querySelectorAll(".line-container");
+    for (let line of lines) {
+        grid.removeChild(line);
+    }
+}
+
+function createGrid() {
+    clearGrid();
+
     for (let i = 0; i < gridSize; i++) {
         let lineContainer = document.createElement("div");
         lineContainer.classList.add("line-container");
@@ -10,9 +24,22 @@ document.addEventListener("DOMContentLoaded", () => {
         for (let j = 0; j < gridSize; j++) {
             let square = document.createElement("div");
             square.classList.add("square");
+            square.addEventListener("mouseover", colorSquare)
             lineContainer.appendChild(square);
         }
 
         grid.appendChild(lineContainer);
     }
-})
+}
+
+function askGridSize() {
+    gridSize = prompt("Enter the grid size :");
+    createGrid();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    createGrid();
+});
+
+setSizeButton.addEventListener("click", askGridSize);
+
